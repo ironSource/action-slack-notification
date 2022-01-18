45f6bf3efc3c7220d304640c3c83ce90e1eddbb2
AndyKIron <andyk@is.com>
2022-01-18 15:49:11 +0200

    feat: remove default icon_url to IS FusionUI icon

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,9 +54,9 @@ send_notification() {
          'attachments': ${attachments}
          }"
 
-#  echo $DATA
+  echo $DATA
 
-  echo "Send: $(curl -X POST -H 'Content-type: application/json' --data "$DATA" "${INPUT_SLACK_WEBHOOK}")"
+#  echo "Send: $(curl -X POST -H 'Content-type: application/json' --data "$DATA" "${INPUT_SLACK_WEBHOOK}")"
 }
 
 main() {
